760fd121e8b1cd82a92b3fa9d25031602aede685
flix2net <119479423+flix2net@users.noreply.github.com>
2023-11-03 15:09:23 +0100

    script.js

--- script.js
+++ script.js
@@ -5,9 +5,60 @@ const searchResults = document.getElementById("searchResults");
 const embedFrame = document.getElementById("embedFrame");
 
 searchButton.addEventListener("click", () => {
-    // Implement API request and display search results here
+    const searchQuery = searchInput.value;
+    const apiUrl = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${searchQuery}`;
+
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // Clear previous search results
+            searchResults.innerHTML = "";
+
+            if (data.results && data.results.length > 0) {
+                data.results.forEach((item) => {
+                    const resultItem = document.createElement("div");
+                    resultItem.classList.add("result-item");
+                    resultItem.setAttribute("data-id", item.id);
+
+                    const title = document.createElement("h3");
+                    title.textContent = item.title || item.name;
+
+                    // Add additional content information, such as poster, description, etc.
+
+                    resultItem.appendChild(title);
+                    searchResults.appendChild(resultItem);
+
+                    // Listen for clicks on result items to show content in the iframe
+                    resultItem.addEventListener("click", () => {
+                        displayContentInIframe(item);
+                    });
+                });
+            } else {
+                searchResults.textContent = "No results found.";
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+            searchResults.textContent = "An error occurred while fetching data.";
+        });
 });
 
-searchResults.addEventListener("click", (event) => {
-    // Implement content embedding in iframes when an item is selected
-});
+function displayContentInIframe(content) {
+    // Make an API request to get more details about the selected content
+    // Then, generate the URL for embedding and set it in the iframe
+    // You can use the content type (movie or TV show) to construct the URL
+    const detailApiUrl = `https://api.themoviedb.org/3/movie/${content.id}?api_key=${apiKey}`;
+
+    fetch(detailApiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.videos && data.videos.results.length > 0) {
+                const videoKey = data.videos.results[0].key;
+                const embedUrl = `https://autoembed.to/movie/tmdb/${content.id}?server=2&video_key=${videoKey}`;
+                embedFrame.src = embedUrl;
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching content details:", error);
+        });
+}
