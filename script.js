798b6436848e39453d68973b9b4d025f188feb1a
flix2net <119479423+flix2net@users.noreply.github.com>
2023-11-03 15:02:13 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const apiKey = "683265ee537211df26d8125e4759ea32";
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+const searchResults = document.getElementById("searchResults");
+const embedFrame = document.getElementById("embedFrame");
+
+searchButton.addEventListener("click", () => {
+    // Implement API request and display search results here
+});
+
+searchResults.addEventListener("click", (event) => {
+    // Implement content embedding in iframes when an item is selected
+});
